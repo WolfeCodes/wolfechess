20f1d59581f85a796a6381cd07307a6c7b18b32b
marcwolfe <marc.wolfe21@gmail.com>
2021-12-13 15:06:45 -0600

    First commit w/ Chess.js and ChessBoardjsx

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ChessBoard from 'chessboardjsx';
+function App() {
+  return (
+    <div className="App">
+      <ChessBoard position = 'start'/>
+    </div>
+  );
+}
+
+export default App;
