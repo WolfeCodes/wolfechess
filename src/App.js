adf224b7afd6241f72a7e0c53b215e439e344025
marcwolfe <marc.wolfe21@gmail.com>
2021-12-15 11:26:38 -0600

    Got the chess pieces to stick in place and to also not allow for illegal moves

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
-import ChessBoard from 'chessboardjsx';
+import React, {useState, useEffect, useRef} from 'react';
+import ChessBoard from "chessboardjsx"
+import Chess from "chess.js";
 function App() {
+
+  const [fen, setFen] = useState ('start')
+
+  let chessGame = useRef(null);
+
+  useEffect(() =>{
+    chessGame.current = new Chess();
+  }, [])
+    //Checking for valid moves 
+  const onDrop = ({sourceSquare, targetSquare}) => {
+    //determines the move of the pieces
+    let move = chessGame.current.move({
+      //Move from one square to next square
+      from: sourceSquare,
+      to: targetSquare
+    })
+    //Not allowing illegal moves
+    if(move === null) return;
+
+    setFen(chessGame.current.fen())
+  } 
+
+  // console.log(move)
+  console.log(chessGame)
+
+
+
+
   return (
     <div className="App">
-      <ChessBoard position = 'start'/>
+      <ChessBoard position= {fen} 
+      onDrop={onDrop}/>
     </div>
   );
 }
